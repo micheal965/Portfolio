8d4aeecc56198f45120e57fc0d95ead2c6d5d97d
micheal965 <micheal.magdy69@yahoo.com>
2026-03-14 08:18:47 +0200

    fix: update main section to use ngClass for opacity control and add animation logic

--- src/app/components/sections/main-section/main-section.component.ts
+++ src/app/components/sections/main-section/main-section.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { ScrollTriggerDirective } from '../../../directives/scroll-trigger.directive';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-main-section',
   standalone: true,
-  imports: [ScrollTriggerDirective],
+  imports: [ScrollTriggerDirective, NgClass],
   templateUrl: './main-section.component.html',
   styleUrl: './main-section.component.css',
 })
 export class MainSectionComponent {
   @Input({ required: true }) title: string = '';
+  shouldAnimate: boolean = true;
+
+  ngOnInit() {
+    if (this.title === 'Projects') this.shouldAnimate = false;
+  }
 }
